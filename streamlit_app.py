6d82ff6498075684a0675d8214d9ee3ba23953af
jamesagooding <80205931+jamesagooding@users.noreply.github.com>
2023-11-30 09:57:50 +0000

    Update streamlit_app.py
    fixed bug in else

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
     if not fruit_choice:
         streamlit.error("please select a fruit to get info")
-    else
+    else:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
         # Use pandas to normalise the fruityvice response and format
         fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
