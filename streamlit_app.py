a22d422ce91623471275e7568eb39c491c5d5c69
jamesagooding <80205931+jamesagooding@users.noreply.github.com>
2023-11-30 10:02:26 +0000

    Update streamlit_app.py
    fixed issue with capitalised F.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # Fruityvice API call function
-def get_Fruityvice_data(this_fruit_choice):
+def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     # Use pandas to normalise the fruityvice response and format
     fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
