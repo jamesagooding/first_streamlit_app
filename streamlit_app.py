4b57b93f3fc53074c82e7ec654b11550b2121ec4
jamesagooding <80205931+jamesagooding@users.noreply.github.com>
2023-11-30 12:21:53 +0000

    Update streamlit_app.py
    added variable to insert statement and close connection stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,21 +56,24 @@ def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * from fruit_load_list")
         return my_cur.fetchall()
+        
 
 if streamlit.button('Get fruit load list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values('from streamlit')")
+        my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values('" + new_fruit + "')")
         return "thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add to the list?')
 if streamlit.button('Add a Fruit to the list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
+    my_cnx.close()
     streamlit.text(back_from_function)
 
 #streamlit.write('Thanks for adding:', add_my_fruit)
